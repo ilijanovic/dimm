e98c32ed7dcda14e08745a91046dd19277269748
ilijanovic <ilija.marijanovic@energie.family>
2023-08-28 21:57:05 +0200

    fix: update

--- output/index.js
+++ output/index.js
@@ -1,15 +1,17 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-const rpio_1 = __importDefault(require("rpio"));
-let pin = 12; /* P12/GPIO18 */
-let range = 1024; /* LEDs can quickly hit max brightness, so only use */
-let max = 128; /*   the bottom 8th of a larger scale */
-let clockdiv = 8; /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
-let interval = 5; /* setInterval timer, speed of pulses */
-let times = 5; /* How many times to pulse before exiting */
+var rpio_1 = __importDefault(require("rpio"));
+var pin = 12; /* P12/GPIO18 */
+var range = 1024; /* LEDs can quickly hit max brightness, so only use */
+var max = 128; /*   the bottom 8th of a larger scale */
+var clockdiv = 8; /* Clock divider (PWM refresh rate), 8 == 2.4MHz */
+var interval = 5; /* setInterval timer, speed of pulses */
+var times = 5; /* How many times to pulse before exiting */
 /*
  * Enable PWM on the chosen pin and set the clock and range.
  */
@@ -19,20 +21,25 @@ rpio_1.default.pwmSetRange(pin, range);
 /*
  * Repeatedly pulse from low to high and back again until times runs out.
  */
-let direction = 1;
-let data = 0;
-let pulse = setInterval(function () {
+var direction = 1;
+var data = 0;
+var pulse = setInterval(
+  function () {
     rpio_1.default.pwmSetData(pin, data);
     if (data === 0) {
-        direction = 1;
-        if (times-- === 0) {
-            clearInterval(pulse);
-            rpio_1.default.open(pin, rpio_1.default.INPUT);
-            return;
-        }
-    }
-    else if (data === max) {
-        direction = -1;
+      direction = 1;
+      if (times-- === 0) {
+        clearInterval(pulse);
+        rpio_1.default.open(pin, rpio_1.default.INPUT);
+        return;
+      }
+    } else if (data === max) {
+      direction = -1;
     }
     data += direction;
-}, interval, data, direction, times);
+  },
+  interval,
+  data,
+  direction,
+  times
+);
